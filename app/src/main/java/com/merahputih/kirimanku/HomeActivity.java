9a4874f0f87765d11e84641832e13d911dfbaf00
Rainer Regan <rainercoc@gmail.com>
2022-04-14 12:52:13 +0700

    Menambahkan halaman Lacak Paket

--- app/src/main/java/com/merahputih/kirimanku/HomeActivity.java
+++ app/src/main/java/com/merahputih/kirimanku/HomeActivity.java
@@ -2,13 +2,38 @@ package com.merahputih.kirimanku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // Views
+    LinearLayout lacakKirimanLayoutButton;
+    LinearLayout cekOngkirLayoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Assign Views
+        lacakKirimanLayoutButton = findViewById(R.id.lacakKirimanLayoutButton);
+        cekOngkirLayoutButton = findViewById(R.id.cekOngkirLayoutButton);
+
+        // Assign On Click Listener
+        lacakKirimanLayoutButton.setOnClickListener(this);
+        cekOngkirLayoutButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == cekOngkirLayoutButton){
+            Toast.makeText(this, "Layanan ini belum tersedia, tunggu update selanjutnya", Toast.LENGTH_SHORT).show();
+        } else if(v == lacakKirimanLayoutButton){
+            startActivity(new Intent(HomeActivity.this, LacakKirimanActivity.class));
+        }
     }
 }
